9369bbbf1f27be31fa69b92bda975916ee9f083b
dskjal <gsmail38484@gmail.com>
2016-04-21 19:56:03 +0900

    add "set face normal" and "set last selected normal"

--- smooth-normal.py
+++ smooth-normal.py
@@ -1,22 +1,33 @@
 import bpy
+import math
 import mathutils
 import copy
+import bmesh
 
 bl_info = {
     "name" : "Normal Smooth Tool",             
     "author" : "dskjal",                  
-    "version" : (1,0),                  
+    "version" : (2,0),                  
     "blender" : (2, 7, 7),              
     "location" : "",   
-    "description" : "Smooth Custom Normal(s)",   
+    "description" : "Edit Custom Normal(s)",   
     "warning" : "",
-    "wiki_url" : "https://github.com/dskjal/Normal-Smooth-Tool",                    
+    "wiki_url" : "https://github.com/dskjal/Smooth-Normal",                    
     "tracker_url" : "",                 
     "category" : "Mesh"                   
 }
 
-def get_normals(data):
-    normals = [mathutils.Vector((0.0,0.0,0.0))]*len(data.vertices)
+def get_vertex_normal(data, index):
+    normal = data.vertices[index].normal
+    if data.has_custom_normals:
+        for l in data.loops:
+            if index == l.vertex_index:
+                return l.normal
+            
+    return normal
+        
+def get_vertex_normals(data):
+    normals = [(0.0,0.0,0.0)]*len(data.vertices)
     if data.has_custom_normals:
         data.calc_normals_split()
         for poly in data.polygons:
@@ -29,9 +40,34 @@ def get_normals(data):
             
     return normals
 
+def get_loop_normals(data):
+    data.calc_normals_split()
+    normals = []
+    for l in data.loops:
+        normals.append(l.normal)
+     
+    return normals
+
+def create_face_table(data):
+    to_faces = []
+    for i in range(0, len(data.vertices)):
+        to_faces.append([])
+        
+    for p in data.polygons:
+        for i in range( p.loop_start, p.loop_start + p.loop_total ):
+            index = data.loops[i].vertex_index
+            to_faces[index].append(i)
+            
+    return to_faces
+
 def smooth_selected_normals(data):
-    normals = get_normals(data)
+    normals = get_loop_normals(data)
+    
     out_normals = copy.deepcopy(normals)  
+    
+    vnormals = get_vertex_normals(data)
+   
+    to_faces = create_face_table(data)  
     
     #create edge table
     edges = []
@@ -41,30 +77,67 @@ def smooth_selected_normals(data):
         vs = e.vertices
         edges[vs[0]].append(vs[1])
         edges[vs[1]].append(vs[0])
+        
+    #smooth normals
+    selected = [v for v in data.vertices if v.select]
+    for v in selected:
+        cn = mathutils.Vector(vnormals[v.index])
+        for e in edges[v.index]:
+            cn += vnormals[e]
+        
+        cn.normalize()
+        for f in to_faces[v.index]:
+            out_normals[f] = cn
+        
+    data.normals_split_custom_set(out_normals)
+    
+def revert_selected_normals(data):
+    normals = get_loop_normals(data)
+            
+    to_faces = create_face_table(data)
+            
+    for v in data.vertices:
+        if v.select:
+            for f in to_faces[v.index]:
+                normals[f] = v.normal
+            
+    data.normals_split_custom_set(normals)
 
+def set_same_normal(data, normal):
+    normals = get_loop_normals(data)  
+    to_faces = create_face_table(data)
+        
     #update normals
     selected = [v for v in data.vertices if v.select]
     for v in selected:
-        cn = mathutils.Vector(normals[v.index])
-        for e in edges[v.index]:
-            cn += normals[e]
-        cn.normalize()
-        out_normals[v.index] = cn
+        for f in to_faces[v.index]:
+            normals[f] = normal
         
-    #set normals
-    data.normals_split_custom_set_from_vertices(out_normals)
-    data.calc_normals_split()
-         
+    data.normals_split_custom_set(normals)
     
-def revert_selected_normals(data):
-    normals = get_normals(data)
+def set_same_average_normal(data):
+    normals = get_vertex_normals(data)
+        
+    #update normals
+    normal = mathutils.Vector((0.0,0.0,0.0))
     for v in data.vertices:
         if v.select:
-            normals[v.index] = v.normal
-            
-    data.normals_split_custom_set_from_vertices(normals)
-    data.calc_normals_split()
-
+            normal += normals[v.index]
+    
+    normal.normalize()
+        
+    set_same_normal(data, normal)
+    
+def set_face_normal(data):
+    normals = get_loop_normals(data)
+    for p in data.polygons:
+        if p.select:
+            normal = p.normal
+            for i in range( p.loop_start, p.loop_start + p.loop_total ):
+              normals[i] = normal    
+    
+    data.normals_split_custom_set(normals)  
+    
 class UI(bpy.types.Panel):
   bl_label = "Normal Edit"
   bl_space_type = "VIEW_3D"
@@ -80,6 +153,10 @@ class UI(bpy.types.Panel):
     ob = context.object
     row = layout.row()
     layout.operator("smoothnormal.smoothnormals")
+    layout.operator("smoothnormal.lastnormal")
+    #layout.operator("smoothnormal.sameaveragenormal");
+    if context.scene.tool_settings.mesh_select_mode[2]:
+        layout.operator("smoothnormal.setfacenormal")
     layout.operator("smoothnormal.revert")
     
 class SmoothButton(bpy.types.Operator):
@@ -96,6 +173,51 @@ class SmoothButton(bpy.types.Operator):
 
     return{'FINISHED'}
 
+class SetSameAverageNormalButton(bpy.types.Operator):
+  bl_idname = "smoothnormal.sameaveragenormal"
+  bl_label = "set average normal"
+  
+  def execute(self, context):
+    o = bpy.context.active_object
+    o.data.use_auto_smooth = True
+    bpy.ops.object.mode_set(mode='OBJECT')
+    set_same_average_normal(o.data)
+    bpy.context.scene.update()
+    bpy.ops.object.mode_set(mode='EDIT')
+
+    return{'FINISHED'}
+
+class SetSameNormalLastButton(bpy.types.Operator):
+    bl_idname = "smoothnormal.lastnormal"
+    bl_label = "set last selected normal"
+    
+    def execute(self, context):
+        o = bpy.context.active_object
+        o.data.use_auto_smooth = True
+            
+        bm = bmesh.from_edit_mesh(o.data)
+        active = bm.select_history.active
+        if active:
+            if bpy.context.scene.tool_settings.mesh_select_mode[0]:
+                bm.verts.ensure_lookup_table()
+            elif bpy.context.scene.tool_settings.mesh_select_mode[1]:
+                bm.edges.ensure_lookup_table()
+            elif bpy.context.scene.tool_settings.mesh_select_mode[2]:
+                bm.faces.ensure_lookup_table()
+            
+            normal = copy.deepcopy(active.normal)
+            index = active.index
+            
+            bpy.ops.object.mode_set(mode='OBJECT')
+            if bpy.context.scene.tool_settings.mesh_select_mode[0]:
+                normal = get_vertex_normal(o.data, index)
+            
+            set_same_normal(o.data, normal)
+            bpy.context.scene.update()
+            bpy.ops.object.mode_set(mode='EDIT')
+
+        return{'FINISHED'}
+    
 class RevertButton(bpy.types.Operator):
     bl_idname = "smoothnormal.revert"
     bl_label = "revert selected normal(s)"
@@ -109,6 +231,20 @@ class RevertButton(bpy.types.Operator):
         bpy.ops.object.mode_set(mode='EDIT')
 
         return{'FINISHED'}
+    
+class SetFaceNormal(bpy.types.Operator):
+    bl_idname = "smoothnormal.setfacenormal"
+    bl_label = "set face normal"
+    
+    def execute(self, context):
+        o = bpy.context.active_object
+        o.data.use_auto_smooth = True
+        bpy.ops.object.mode_set(mode='OBJECT')
+        set_face_normal(o.data)
+        bpy.context.scene.update()
+        bpy.ops.object.mode_set(mode='EDIT')
+        
+        return {'FINISHED'}
     
 def register():
     bpy.utils.register_module(__name__)
